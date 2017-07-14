a5ce3e48540a2c92170f97b5a5ca3a310dd8c463
FdelMazo <federicodelmazo@hotmail.com>
2017-07-14 11:30:49 -0300

    Minor

--- CoversDownloader.py
+++ CoversDownloader.py
@@ -9,6 +9,33 @@ import shutil
 
 INVALID_CHARACTERS = '/?<>\:*|" ' #Windows
 
+def solicitations():
+	solicits = input("\n Write a solicitation with COMPANY, MONTH, YEAR: ")
+	link = cbr_solicitation_crawler(solicits)
+	soup = request_soup(link.get('href'))
+	if soup:
+		dir_name=soup.title.string
+		os.makedirs(dir_name,exist_ok=True)
+		images = []
+		for div in soup.findAll("div",{"class":"responsiveImg img_article_content"}):
+			comic_name = str(div.next_sibling.next_sibling.next_sibling.string)
+			comic_name = ''.join(list(c if c not in INVALID_CHARACTERS else "_" for c in comic_name)).title()
+			comic_name+=".jpg"
+			for picture in div.picture.findAll("source"):
+				img = picture["srcset"]
+				index = img.index(".jpg") +4
+				img = img[:index]
+				if comic_name != "None.jpg" and (comic_name,img) not in images: images.append((comic_name,img))
+		for comic_name,img in images:
+			r = requests.get(img)
+			with open(comic_name, "wb") as f:
+				f.write(r.content)
+			print("Downloaded: {}".format(comic_name))
+			try:
+				shutil.move(comic_name, dir_name)
+			except:
+				pass
+				
 def get_cbseries(wikis, search_type, search_terms, any_all):
 	possible_links, counter = [], 1
 	for wiki in wikis:
@@ -64,7 +91,7 @@ def get_cover_type(wiki, cbseries):
 
 def character_or_series(search_type, any_all, term):
 	search_terms = input("\n Write a comicbook {}: ".format(term))
-	if search_type=="Series" or search_type=="Character":
+	if search_type=="Volume":
 		wikis = get_wikis(True)
 	elif search_type=="Artist":
 		wikis = get_wikis(False)
@@ -93,35 +120,7 @@ def series():
 	character_or_series("Volume", all, "Series")
 
 def artist():
-	character_or_series("Artist", all, "Artist")		
-	
-def solicitations():
-	solicits = input("\n Write a solicitation with COMPANY, MONTH, YEAR: ")
-	link = cbr_solicitation_crawler(solicits)
-	soup = request_soup(link.get('href'))
-	if soup:
-		dir_name=soup.title.string
-		os.makedirs(dir_name,exist_ok=True)
-		images = []
-		for div in soup.findAll("div",{"class":"responsiveImg img_article_content"}):
-			comic_name = str(div.next_sibling.next_sibling.next_sibling.string)
-			comic_name = ''.join(list(c if c not in INVALID_CHARACTERS else "_" for c in comic_name)).title()
-			comic_name+=".jpg"
-			for picture in div.picture.findAll("source"):
-				img = picture["srcset"]
-				index = img.index(".jpg") +4
-				img = img[:index]
-				if (comic_name,img) not in images: images.append((comic_name,img))
-		for comic_name,img in images:
-			r = requests.get(img)
-			with open(comic_name, "wb") as f:
-				f.write(r.content)
-			print("Downloaded: {}".format(comic_name))
-			try:
-				shutil.move(comic_name, dir_name)
-			except:
-				pass			
-	
+	character_or_series("Artist", all, "Artist")					
 """UNIVERSAL"""				
 def menu():
 	functions = {}
